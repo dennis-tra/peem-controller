f319940b9a82a9e95bd4010adcb588e824a67d81
Dennis Trautwein <dennis.trautwein@posteo.de>
2017-02-09 16:45:17 +0100

    Add two new constants

--- src/main/java/de/agbauer/physik/Generic/Constants.java
+++ src/main/java/de/agbauer/physik/Generic/Constants.java
@@ -4,7 +4,9 @@ package de.agbauer.physik.Generic;
  * Created by dennis on 03/02/2017.
  */
 public class Constants {
+    public static boolean peemConnected = false;
     public static String defaultPort = "COM3";
     //public static String cameraDevice = "pco_camera";
     public static String cameraDevice = "Camera";
+    public static String defaultFileSaveFolder = "/Users/dennis/Documents/github/peem-controller/";
 }
