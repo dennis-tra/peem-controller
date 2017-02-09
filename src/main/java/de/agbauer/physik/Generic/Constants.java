c74033bc1c786f5bfb20f2cff00a2f75a8c681ce
Dennis Trautwein <dennis.trautwein@posteo.de>
2017-02-09 11:12:41 +0100

    Add peer specific constants

---
+++ src/main/java/de/agbauer/physik/Generic/Constants.java
@@ -0,0 +1,10 @@
+package de.agbauer.physik.Generic;
+
+/**
+ * Created by dennis on 03/02/2017.
+ */
+public class Constants {
+    public static String defaultPort = "COM3";
+    //public static String cameraDevice = "pco_camera";
+    public static String cameraDevice = "Camera";
+}
