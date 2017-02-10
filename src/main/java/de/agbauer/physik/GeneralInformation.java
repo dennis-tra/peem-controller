4fe70644e723f91b69271aed720429286822899e
Dennis Trautwein <dennis.trautwein@posteo.de>
2017-02-10 18:11:08 +0100

    Add general information class

---
+++ src/main/java/de/agbauer/physik/GeneralInformation.java
@@ -0,0 +1,10 @@
+package de.agbauer.physik;
+
+/**
+ * Created by dennis on 10/02/2017.
+ */
+public interface GeneralInformation {
+    String getProbeName();
+    String getExcitation();
+    String getAperture();
+}
