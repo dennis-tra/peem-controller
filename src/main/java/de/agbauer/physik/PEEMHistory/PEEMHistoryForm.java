466469b3e1963fa12e30ef3f5d1eeb4604ccc045
Dennis Trautwein <dennis.trautwein@posteo.de>
2017-02-10 18:14:46 +0100

    Add peem history class

---
+++ src/main/java/de/agbauer/physik/PEEMHistory/PEEMHistoryForm.java
@@ -0,0 +1,14 @@
+package de.agbauer.physik.PEEMHistory;
+
+import javax.swing.*;
+
+/**
+ * Created by dennis on 10/02/2017.
+ */
+public class PEEMHistoryForm {
+    private JTable table1;
+    private JTable table2;
+    private JComboBox comboBox1;
+    private JPanel peemHistoryPanel;
+    private JPanel panel;
+}
