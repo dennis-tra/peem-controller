f04fe4dafd2585d315f012293dd0d6b599c5eeed
Dennis Trautwein <dennis.trautwein@posteo.de>
2017-02-10 18:15:32 +0100

    Add aperture and probe name fields

--- src/main/java/de/agbauer/physik/MainWindow.java
+++ src/main/java/de/agbauer/physik/MainWindow.java
@@ -2,6 +2,7 @@ package de.agbauer.physik;
 
 import de.agbauer.physik.OptimisationSeries.OptimisationSeriesForm;
 import de.agbauer.physik.PEEMCommunicator.RxTxConnectionHandler;
+import de.agbauer.physik.PEEMHistory.PEEMHistoryForm;
 import de.agbauer.physik.PEEMState.PEEMStatePanel;
 import de.agbauer.physik.QuickAcquisition.QuickAcquistionForm;
 import gnu.io.CommPortIdentifier;
@@ -16,15 +17,26 @@ public class MainWindow extends JFrame {
     private JPanel rootPanel;
     JLabel statusBarLabel;
     OptimisationSeriesForm optimisationSeriesForm;
-    private JTextField textField1;
+    JTextField probeNameTextField;
     PEEMStatePanel peemStatePanel;
     QuickAcquistionForm quickAcquistionForm;
+    JComboBox apertureComboBox;
+    private PEEMHistoryForm PEEMHistoryForm1;
+    JTextField excitationTextField;
+    private JTextArea textArea1;
+    private JTabbedPane tabbedPane1;
 
     MainWindow() {
         super();
 
         setContentPane(rootPanel);
         pack();
+
+        apertureComboBox.addItem("50");
+        apertureComboBox.addItem("75");
+        apertureComboBox.addItem("500");
+        apertureComboBox.addItem("1500");
+        apertureComboBox.setSelectedItem("500");
 
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
