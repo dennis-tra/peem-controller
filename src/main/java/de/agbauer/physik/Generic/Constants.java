59378e7be2d3bfc317947d7b4393c8e633b7cda4
Dennis Trautwein <dennis.trautwein@posteo.de>
2017-02-10 19:00:34 +0100

    Change constants to final

--- src/main/java/de/agbauer/physik/Generic/Constants.java
+++ src/main/java/de/agbauer/physik/Generic/Constants.java
@@ -4,10 +4,10 @@ package de.agbauer.physik.Generic;
  * Created by dennis on 03/02/2017.
  */
 public class Constants {
-    public static String version = "v0.0.1";
-    public static boolean peemConnected = false;
-    public static String defaultPort = "COM3";
+    public static final String version = "v0.0.1";
+    public static final boolean peemConnected = false;
+    public static final String defaultPort = "COM3";
     //public static String cameraDevice = "pco_camera";
-    public static String cameraDevice = "Camera";
-    public static String defaultFileSaveFolder = "/Users/dennis/Documents/github/peem-controller/"; // Must have trailing (back)slash
+    public static final String cameraDevice = "Camera";
+    public static final String defaultFileSaveFolder = "/Users/dennis/Documents/github/peem-controller/"; // Must have trailing (back)slash
 }
