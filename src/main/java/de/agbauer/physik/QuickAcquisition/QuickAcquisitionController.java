c05b26d7591e6e62c440b995a4ff798f40d94c65
Dennis Trautwein <dennis.trautwein@posteo.de>
2017-02-17 18:34:39 +0100

    Refactor Quickacquisition controller

--- src/main/java/de/agbauer/physik/QuickAcquisition/QuickAcquisitionController.java
+++ src/main/java/de/agbauer/physik/QuickAcquisition/QuickAcquisitionController.java
@@ -2,21 +2,20 @@ package de.agbauer.physik.QuickAcquisition;
 
 import de.agbauer.physik.GeneralInformation.GeneralInformationChangeListener;
 import de.agbauer.physik.GeneralInformation.GeneralInformationData;
-import de.agbauer.physik.Generic.ActivatableForm;
 import de.agbauer.physik.Generic.Constants;
 import ij.ImagePlus;
 import org.micromanager.SnapLiveManager;
 import org.micromanager.Studio;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
+import java.io.IOException;
 import java.util.*;
 import java.util.logging.Logger;
 
 public class QuickAcquisitionController extends Observable implements GeneralInformationChangeListener {
     private Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
     private FileSaver fileSaver;
-    private final QuickAcquisitionForm form;
+    private QuickAcquisitionForm form;
     private Studio studio;
     private SnapLiveManager snapLiveManager;
     private GeneralInformationData generalInformationData;
@@ -27,28 +26,68 @@ public class QuickAcquisitionController extends Observable implements GeneralInf
         this.studio = studio;
         this.snapLiveManager = studio == null ? null : studio.getSnapLiveManager();
 
-        this.form.liveButton.addActionListener(this::liveMode);
-        this.form.snapButton.addActionListener(this::snapImage);
-        this.form.snapPlusButton.addActionListener(this::snapPlusImage);
         this.form.stopButton.addActionListener(e -> turnOffLiveMode());
+
+        this.form.liveButton.addActionListener(e -> acquireImage(form.liveTextField, form.liveComboBox, new LiveAcquisition()));
+        this.form.snapButton.addActionListener(e -> acquireImage(form.snapTextField, form.snapComboBox, new SnapAcquisition()));
+        this.form.snapPlusButton.addActionListener(e -> acquireImage(form.snapPlusTextField, form.snapPlusComboBox, new SnapPlusAcquisition()));
+
     }
 
-    private void snapPlusImage(ActionEvent actionEvent) {
 
-        logger.info("Acquiring image...");
-
-        notifyObservers("started-acquisition");
-
-        turnOffLiveMode();
-
-        String exposureStr = this.form.snapPlusTextField.getText();
-
-        float exposureInMs = Float.parseFloat(exposureStr);
-        int binning = Integer.parseInt((String) this.form.snapPlusComboBox.getSelectedItem());
+    private void acquireImage(JTextField exposureTextField, JComboBox binningComboBox, AcquisitionAction acquisitionAction) {
+        logger.info("Starting image acquisition...");
 
         try {
-            setExposure(exposureInMs);
-            setCameraBinning(binning);
+            turnOffLiveMode();
+
+            float exposureInMs = Float.parseFloat(exposureTextField.getText());
+            int binning = Integer.parseInt((String) binningComboBox.getSelectedItem());
+
+            logger.info("Settting camera exposure to " + exposureInMs/1000 + " s");
+            studio.getCMMCore().setExposure(exposureInMs);
+
+            logger.info("Setting camera binning to " + binning);
+            studio.getCMMCore().setProperty(Constants.cameraDevice, "Binning", binning);
+
+            if (snapLiveManager.getDisplay() != null) {
+                double zoomFactor = zoomForBinning(binning);
+                snapLiveManager.getDisplay().setZoom(zoomFactor);
+            }
+
+            acquisitionAction.acquire(exposureInMs, binning);
+
+        } catch(NumberFormatException exc) {
+            logger.warning("Couldn't read your quick acquisition input values: " + exc.getMessage());
+        } catch (Exception e1) {
+            logger.severe("Couldn't snap an image: " + e1.getMessage());
+        }
+    }
+
+    private interface AcquisitionAction {
+        void acquire(float exposureInMs, int binning) throws IOException;
+    }
+
+    private class SnapAcquisition implements AcquisitionAction {
+
+        @Override
+        public void acquire(float exposureInMs, int binning) {
+            snapLiveManager.snap(true);
+        }
+    }
+
+    private class LiveAcquisition implements AcquisitionAction {
+
+        @Override
+        public void acquire(float exposureInMs, int binning) {
+            snapLiveManager.setLiveMode(true);
+        }
+    }
+
+    private class SnapPlusAcquisition implements AcquisitionAction {
+
+        @Override
+        public void acquire(float exposureInMs, int binning) throws IOException {
 
             snapLiveManager.snap(true).get(0);
             ImagePlus imagePlus = snapLiveManager.getDisplay().getImagePlus();
@@ -56,65 +95,14 @@ public class QuickAcquisitionController extends Observable implements GeneralInf
             int dialogResult = JOptionPane.showConfirmDialog(null, "Do you want to save this image?", "Save image dialog", JOptionPane.YES_NO_OPTION);
 
             if(dialogResult == JOptionPane.YES_OPTION) {
-                logger.info("Saving image...");
-                fileSaver.save(generalInformationData, imagePlus, exposureStr);
+
+                fileSaver.save(generalInformationData, imagePlus, "" + exposureInMs);
                 setChanged();
                 notifyObservers(imagePlus);
+
             } else {
                 logger.info("User denied saving image");
             }
-
-        } catch (Exception e1) {
-            logger.severe("Couldn't snap an image: " + e1.getMessage());
-        } finally {
-            notifyObservers("finished-acquisition");
-        }
-    }
-
-    private void snapImage(ActionEvent actionEvent) {
-        logger.info("Acquiring image...");
-        setChanged();
-        notifyObservers("started-acquisition");
-
-        turnOffLiveMode();
-
-        String exposureStr = this.form.snapTextField.getText();
-
-        float exposureInMs = Float.parseFloat(exposureStr);
-        int binning = Integer.parseInt((String) this.form.snapComboBox.getSelectedItem());
-
-        try {
-            setExposure(exposureInMs);
-            setCameraBinning(binning);
-            snapLiveManager.snap(true);
-
-        } catch(NumberFormatException exc) {
-            logger.warning("Couldn't read your quick acquisition input values");
-        } catch (Exception e1) {
-            logger.severe("Couldn't snap an image : " + e1.getMessage());
-        } finally {
-            setChanged();
-            notifyObservers("finished-acquisition");
-        }
-    }
-
-    private void liveMode(ActionEvent e) {
-        logger.info("Turning on Live mode");
-
-        turnOffLiveMode();
-
-        try {
-            float exposureInMs = Float.parseFloat(this.form.liveTextField.getText());
-            int binning = Integer.parseInt((String) this.form.liveComboBox.getSelectedItem());
-
-            setExposure(exposureInMs);
-            setCameraBinning(binning);
-            snapLiveManager.setLiveMode(true);
-
-        } catch(NumberFormatException exc) {
-            logger.warning("Couldn't read your quick acquisition input values");
-        } catch (Exception e1) {
-            logger.severe("Couldn't turn on live mode: " + e1.getMessage());
         }
     }
 
@@ -126,19 +114,24 @@ public class QuickAcquisitionController extends Observable implements GeneralInf
         }
     }
 
-    private void setCameraBinning(int binning) throws Exception {
-        logger.info("Set camera binning to " + binning);
-        studio.getCMMCore().setProperty(Constants.cameraDevice, "Binning", binning);
-    }
-
-    private void setExposure(double exposureTimeInMs) throws Exception {
-        logger.info("Set camera exposure to " + exposureTimeInMs/1000 + " s");
-        studio.getCMMCore().setExposure(exposureTimeInMs);
-    }
-
     @Override
     public void generalInformationChanged(GeneralInformationData data) {
         this.generalInformationData = data;
         this.form.setGeneralInformationGiven(data.isValid());
+    }
+
+    private double zoomForBinning(int binning) {
+        switch (binning) {
+            case 1:
+                return 0.75;
+            case 2:
+                return 1.5;
+            case 4:
+                return 3.0;
+            case 8:
+                return 6.0;
+            default:
+                return 1.0;
+        }
     }
 }
