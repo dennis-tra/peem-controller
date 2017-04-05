d7757fb13efc33f664208dfcf39a209ca1c033f5
Dennis Trautwein <dennis.trautwein@posteo.de>
2017-04-05 14:54:05 +0200

    Remove Slack token from constants

--- src/main/java/de/agbauer/physik/Generic/Constants.java
+++ src/main/java/de/agbauer/physik/Generic/Constants.java
@@ -6,5 +6,5 @@ public class Constants {
     public static final String defaultPort = "COM3";
     public static final String cameraDevice = System.getenv("CAMERA_DEVICE") == null ? "Camera" : System.getenv("CAMERA_DEVICE");
     public static final String defaultFileSaveFolder = "/Users/dennis/Documents/github/peem-controller/"; // Must have trailing (back)slash
-    public static final String slackBotToken = "xoxb-164035809249-c0bDxvp8yamlHEDQFzii336U";
+    public static final String slackBotToken = "SLACK-TOKEN";
 }
