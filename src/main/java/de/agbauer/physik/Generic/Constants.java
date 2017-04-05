fdeccdf93053f954966eff25f9d2022f9d255c2e
Dennis Trautwein <dennis.trautwein@posteo.de>
2017-04-05 14:56:34 +0200

    Bump version number

--- src/main/java/de/agbauer/physik/Generic/Constants.java
+++ src/main/java/de/agbauer/physik/Generic/Constants.java
@@ -1,8 +1,8 @@
 package de.agbauer.physik.Generic;
 
 public class Constants {
-    public static final String version = "v0.0.1";
     public static final boolean peemConnected = System.getenv("PEEM_CONNECTED") == "TRUE";
+    public static final String version = "v0.1.0";
     public static final String defaultPort = "COM3";
     public static final String cameraDevice = System.getenv("CAMERA_DEVICE") == null ? "Camera" : System.getenv("CAMERA_DEVICE");
     public static final String defaultFileSaveFolder = "/Users/dennis/Documents/github/peem-controller/"; // Must have trailing (back)slash
