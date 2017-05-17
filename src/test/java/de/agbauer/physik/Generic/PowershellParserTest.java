6343221e27d36e58116350b64a62005dc5fdb97c
Dennis Trautwein <dennis.trautwein@posteo.de>
2017-05-17 09:28:27 +0200

    Rename file

--- src/test/java/de/agbauer/physik/Generic/PowershellParserTest.java
+++ src/test/java/de/agbauer/physik/Generic/PowershellParserTest.java
@@ -9,7 +9,7 @@ import java.util.Date;
 
 import static org.junit.Assert.*;
 
-public class AcquisitionParameterParserTest {
+public class PowershellParserTest {
     private AcquisitionParameters ap;
 
     @Before
@@ -19,48 +19,48 @@ public class AcquisitionParameterParserTest {
 
     @Test
     public void parsesApertureValue() throws Exception {
-        ap = AcquisitionParameterParser.parseLine(this.ap, "ContrastAperture 500               ");
+        ap = PowershellParser.parseLine(this.ap, "ContrastAperture 500               ");
         assertEquals("500", ap.aperture);
 
-        ap = AcquisitionParameterParser.parseLine(this.ap, "ContrastAperture 800");
+        ap = PowershellParser.parseLine(this.ap, "ContrastAperture 800");
         assertEquals("800", ap.aperture);
 
         ap.aperture = null;
-        ap = AcquisitionParameterParser.parseLine(this.ap, "ContrastAperture ");
+        ap = PowershellParser.parseLine(this.ap, "ContrastAperture ");
         assertEquals(null, ap.aperture);
 
         ap.aperture = null;
-        ap = AcquisitionParameterParser.parseLine(this.ap, "ContrastAperture");
+        ap = PowershellParser.parseLine(this.ap, "ContrastAperture");
         assertEquals(null, ap.aperture);
 
         ap.aperture = null;
-        ap = AcquisitionParameterParser.parseLine(this.ap, "ContrastAperture 800 +3");
+        ap = PowershellParser.parseLine(this.ap, "ContrastAperture 800 +3");
         assertEquals("800 +3", ap.aperture);
     }
 
     @Test
     public void parsesExcitationValue() throws Exception {
-        ap = AcquisitionParameterParser.parseLine(this.ap, "Excitation       Hg                ");
+        ap = PowershellParser.parseLine(this.ap, "Excitation       Hg                ");
         assertEquals("Hg", ap.excitation);
 
         ap.excitation = null;
-        ap = AcquisitionParameterParser.parseLine(this.ap, "Excitation       210p  ");
+        ap = PowershellParser.parseLine(this.ap, "Excitation       210p  ");
         assertEquals("210p", ap.excitation);
 
         ap.excitation = null;
-        ap = AcquisitionParameterParser.parseLine(this.ap, "Excitation       210p + 410vH  ");
+        ap = PowershellParser.parseLine(this.ap, "Excitation       210p + 410vH  ");
         assertEquals("210p + 410vH", ap.excitation);
     }
 
     @Test
     public void parsesSampleNameValue() throws Exception {
-        ap = AcquisitionParameterParser.parseLine(this.ap, "Sample           Si_300SiO2_MoS2_P9");
+        ap = PowershellParser.parseLine(this.ap, "Sample           Si_300SiO2_MoS2_P9");
         assertEquals("Si_300SiO2_MoS2_P9", ap.sampleName);
     }
 
     @Test
     public void parsesExposureValue() throws Exception {
-        ap = AcquisitionParameterParser.parseLine(this.ap, "Integration      120s              ");
+        ap = PowershellParser.parseLine(this.ap, "Integration      120s              ");
         assertEquals("120s", ap.exposure);
 
     }
@@ -68,58 +68,58 @@ public class AcquisitionParameterParserTest {
     @Test
     public void parsesDateValue() throws Exception {
         Date date = new SimpleDateFormat("yyyyMMdd-HHmm").parse("20170206-1723");
-        ap = AcquisitionParameterParser.parseLine(this.ap, "Run on 20170206-1723");
+        ap = PowershellParser.parseLine(this.ap, "Run on 20170206-1723");
         assertEquals(date.compareTo(ap.createdAt), 0);
 
     }
 
     @Test
     public void parsesImageNumber() throws Exception {
-        ap = AcquisitionParameterParser.parseLine(this.ap, "Imagenr          01                ");
+        ap = PowershellParser.parseLine(this.ap, "Imagenr          01                ");
         assertEquals(ap.imageNumber, 1);
-        ap = AcquisitionParameterParser.parseLine(this.ap, "Imagenr          13                ");
+        ap = PowershellParser.parseLine(this.ap, "Imagenr          13                ");
         assertEquals(ap.imageNumber, 13);
 
         ap.imageNumber = 0;
-        ap = AcquisitionParameterParser.parseLine(this.ap, "Imagenr                          ");
+        ap = PowershellParser.parseLine(this.ap, "Imagenr                          ");
         assertEquals(ap.imageNumber, 0);
 
         ap.imageNumber = 0;
-        ap = AcquisitionParameterParser.parseLine(this.ap, "Imagenr         asdf                 ");
+        ap = PowershellParser.parseLine(this.ap, "Imagenr         asdf                 ");
         assertEquals(ap.imageNumber, 0);
     }
 
     @Test
     public void parsesNote() throws Exception {
-        ap = AcquisitionParameterParser.parseLine(this.ap, "Note                               ");
+        ap = PowershellParser.parseLine(this.ap, "Note                               ");
         assertEquals(null, ap.note);
 
-        ap = AcquisitionParameterParser.parseLine(this.ap, "Note          this is a test ");
+        ap = PowershellParser.parseLine(this.ap, "Note          this is a test ");
         assertEquals("this is a test", ap.note);
     }
 
     @Test
     public void parsesExtractorValues() throws Exception {
-        ap = AcquisitionParameterParser.parseLine(this.ap, "extractor   12502,2  40300");
+        ap = PowershellParser.parseLine(this.ap, "extractor   12502,2  40300");
         assertEquals(12502.2, ap.getExtractorU(), 0.01);
         assertEquals(40300, ap.extractorI, 0.01);
 
         ap.setExtractorU(0.0);
         ap.extractorI = 0.0;
-        ap = AcquisitionParameterParser.parseLine(this.ap, "extractor   ");
+        ap = PowershellParser.parseLine(this.ap, "extractor   ");
         assertEquals(0.0, ap.getExtractorU(), 0.01);
         assertEquals(0.0, ap.extractorI, 0.01);
 
         ap.setExtractorU(0.0);
         ap.extractorI = 0.0;
-        ap = AcquisitionParameterParser.parseLine(this.ap, "extractor  32.4 533,3 ");
+        ap = PowershellParser.parseLine(this.ap, "extractor  32.4 533,3 ");
         assertEquals(32.4, ap.getExtractorU(), 0.01);
         assertEquals(533.3, ap.extractorI, 0.01);
     }
 
     @Test
     public void parsesStigmatorValues() throws Exception {
-        ap = AcquisitionParameterParser.parseLine(this.ap, "stigmator -2,883 -0,003 0,06  0");
+        ap = PowershellParser.parseLine(this.ap, "stigmator -2,883 -0,003 0,06  0");
         assertEquals(-2.883, ap.getStigmatorVx(), 0.01);
         assertEquals(-0.003, ap.getStigmatorVy(), 0.01);
         assertEquals(0.06, ap.getStigmatorSx(), 0.01);
