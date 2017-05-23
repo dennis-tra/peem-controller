6275ac199bfeb92a90b21faa063173b714158c69
Dennis Trautwein <dennis.trautwein@posteo.de>
2017-05-23 14:23:12 +0200

    Add peem computer constants

--- src/main/java/de/agbauer/physik/Constants.java
+++ src/main/java/de/agbauer/physik/Constants.java
@@ -4,9 +4,9 @@ public class Constants {
     public static final boolean peemConnected = System.getenv("PEEM_CONNECTED") == "TRUE";
     public static final String version = "v0.2.0";
     public static final String defaultPort = "COM3";
-    public static final String cameraDevice = System.getenv("CAMERA_DEVICE") == null ? "Camera" : System.getenv("CAMERA_DEVICE");
-    public static final String defaultFileSaveFolder = "/Users/dennis/Documents/github/peem-controller/"; // Must have trailing (back)slash
+    public static final String cameraDevice = "pco_camera";
+    public static final String defaultFileSaveFolder = "F:\\PEEM Messdaten\\"; // Must have trailing (back)slash
     public static final String slackBotToken = "SLACK-TOKEN";
-    public static final String defaultPresetSaveFolder = "/Users/dennis/Documents/github/peem-controller/presets/"; // Must have trailing (back)slash
-    public static final String defaultLogFolder = "/Users/dennis/Documents/github/peem-controller/logs/"; // Must have trailing (back)slash
+    public static final String defaultPresetSaveFolder = "F:\\PEEM Messdaten\\peem-controller-presets\\"; // Must have trailing (back)slash
+    public static final String defaultLogFolder = "F:\\PEEM Messdaten\\peem-controller-logs\\"; // Must have trailing (back)slash
 }
