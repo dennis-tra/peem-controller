29cf531084a9af9437a261d5901d26b7118aaa4a
Dennis Trautwein <dennis.trautwein@posteo.de>
2017-07-21 13:05:59 +0200

    Bump Version number

--- src/main/java/de/agbauer/physik/Constants.java
+++ src/main/java/de/agbauer/physik/Constants.java
@@ -2,11 +2,11 @@ package de.agbauer.physik;
 
 public class Constants {
     public static final boolean peemConnected = System.getenv("PEEM_CONNECTED") == "TRUE";
-    public static final String version = "v0.2.0";
+    public static final String version = "v0.4.0";
     public static final String defaultPort = "COM3";
-    public static final String cameraDevice = "pco_camera";
+    public static final String cameraDevice = "Camera";
     public static final String defaultFileSaveFolder = "F:\\PEEM Messdaten\\"; // Must have trailing (back)slash
-    public static final String slackBotToken = "SLACK-TOKEN";
+    public static final String slackBotToken = "xoxb-164035809249-YC5fdrbc2d9U24muY5xFQpAb";
     public static final String defaultPresetSaveFolder = "F:\\PEEM Messdaten\\peem-controller-presets\\"; // Must have trailing (back)slash
     public static final String defaultLogFolder = "F:\\PEEM Messdaten\\peem-controller-logs\\"; // Must have trailing (back)slash
 }
