0f98b006d9c4e8a4845b3d41d417c1c2ad8aea82
Dennis Trautwein <dennis.trautwein@posteo.de>
2018-02-07 11:42:13 +0100

    Merge Constants

--- src/main/java/de/agbauer/physik/Constants.java
+++ src/main/java/de/agbauer/physik/Constants.java
@@ -4,10 +4,10 @@ public class Constants {
     public static final boolean peemConnected = System.getenv("PEEM_CONNECTED") == "TRUE";
     public static final String version = "v0.5.0";
     public static final String defaultPort = "COM3";
-    public static final String cameraDevice = "pco_camera";
-    public static final String defaultFileSaveFolder = "F:\\PEEM Messdaten\\"; // Must have trailing (back)slash
+    public static final String cameraDevice = "Camera";
+    public static final String defaultFileSaveFolder = "/Users/dennis/Documents/github/peem-controller/"; // Must have trailing (back)slash
     public static final String slackBotToken = "xoxb-164035809249-fabo7Y9Ojj7GgreNzfbZGRCp";
-    public static final String defaultPresetSaveFolder = "F:\\PEEM Messdaten\\peem-controller-presets\\"; // Must have trailing (back)slash
-    public static final String defaultLogFolder = "F:\\PEEM Messdaten\\peem-controller-logs\\"; // Must have trailing (back)slash
-    public static final String pathSeparator = "\\";
+    public static final String defaultPresetSaveFolder = "/Users/dennis/Documents/github/peem-controller/peem-controller-presets/"; // Must have trailing (back)slash
+    public static final String defaultLogFolder = "/Users/dennis/Documents/github/peem-controller/peem-controller-logs/"; // Must have trailing (back)slash
+    public static final String pathSeparator = "/";
 }
