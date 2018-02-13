baa6cf5b4c842ec30bc24c307896cbf2de06c45d
Dennis Trautwein <dennis.trautwein@posteo.de>
2018-02-13 16:08:04 +0100

    Add Area mean plugin

---
+++ src/main/java/de/agbauer/physik/AreaMeanInspectorPlugin/AreaMeanInspectorPanel.java
@@ -0,0 +1,73 @@
+package de.agbauer.physik.AreaMeanInspectorPlugin;
+
+import com.google.common.eventbus.Subscribe;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.Plot;
+import org.micromanager.Studio;
+import org.micromanager.data.Datastore;
+import org.micromanager.data.Image;
+import org.micromanager.data.NewImageEvent;
+import org.micromanager.display.DataViewer;
+import org.micromanager.display.Inspector;
+import org.micromanager.display.InspectorPanel;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class AreaMeanInspectorPanel extends InspectorPanel {
+    public AreaMeanInspectorPanel(Studio studio) {
+
+        Font font = countLabel.getFont();
+        countLabel.setFont(new Font(font.getName(), font.getStyle(), 70));
+        this.add(countLabel);
+
+    }
+
+    private JLabel countLabel = new JLabel("0");
+    private Datastore datastore;
+
+    public JPopupMenu getGearMenu() {
+        return null;
+    }
+
+    public void setInspector(Inspector inspector) {
+    }
+
+    public boolean getIsValid(DataViewer viewer) {
+        return true;
+    }
+
+    public void setDataViewer(DataViewer viewer) {
+        if (viewer == null) {
+            if (this.datastore != null) {
+                this.datastore.unregisterForEvents(this);
+            }
+        } else {
+            this.datastore = viewer.getDatastore();
+            this.datastore.registerForEvents(this);
+        }
+    }
+
+    public boolean getGrowsVertically() {
+        return true;
+    }
+
+    @Override
+    public void cleanup() {
+
+    }
+
+    @Subscribe
+    public void onNewImage(NewImageEvent event) {
+        Image newImage = event.getImage();
+        short[] pixels = (short[]) newImage.getRawPixels();
+
+        int max = 0;
+        for (short pixel: pixels) {
+            max = Math.max(pixel, max);
+        }
+
+        countLabel.setText("" + max);
+
+    }
+}
---
+++ src/main/java/de/agbauer/physik/AreaMeanInspectorPlugin/AreaMeanPlugin.java
@@ -0,0 +1,43 @@
+package de.agbauer.physik.AreaMeanInspectorPlugin;
+
+import org.micromanager.Studio;
+import org.micromanager.display.InspectorPanel;
+import org.micromanager.display.InspectorPlugin;
+import org.scijava.plugin.Plugin;
+import org.scijava.plugin.SciJavaPlugin;
+
+@Plugin(type = InspectorPlugin.class)
+public class AreaMeanPlugin implements InspectorPlugin, SciJavaPlugin {
+
+    private Studio studio;
+
+    @Override
+    public InspectorPanel createPanel() {
+        return new AreaMeanInspectorPanel(studio);
+    }
+
+    @Override
+    public void setContext(Studio studio) {
+        this.studio = studio;
+    }
+
+    @Override
+    public String getName() {
+        return "Max Count";
+    }
+
+    @Override
+    public String getHelpText() {
+        return null;
+    }
+
+    @Override
+    public String getVersion() {
+        return "v0.0.1";
+    }
+
+    @Override
+    public String getCopyright() {
+        return "Christian-Albrechts-Universität zu Kiel, Germany, 2017. Author: Dennis Trautwein";
+    }
+}
